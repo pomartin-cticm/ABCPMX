8e709715f1cfb985736ee57bedc6e6efd3be0422
Guillaume DELACOURT <gdelacourt@cticm.com>
2023-06-08 10:33:46 +0200

    Création de la fenetre etaiement

---
+++ PMXInterface/Fenetres/Filles/Frm_Etaiement.vb
@@ -0,0 +1,103 @@
+﻿Imports PMXMoteur2
+Imports System.IO
+
+Public Class Frm_Etaiement
+
+#Region " Variables locales "
+
+    Dim lBuild As Boolean = True
+
+#End Region
+
+#Region "===OUVERTURE==="
+
+    Public Sub InitialiserFenetre()
+        GestionLangues()
+        GestionStyle()
+        GestionUnites()
+        AfficherPoutreEnCours()
+    End Sub
+
+    Private Sub GestionLangues()
+        If File.Exists(LogicielFichiers.Langue) Then
+
+            Dim Bloc As New Dictionary(Of String, String)
+            Dim BlocLine As New Cls_LinesOfFile(LogicielFichiers.Langue, "#FRM_BASIC")
+            BlocLine.CreationBloc(Bloc)
+
+            Try
+
+                '=== MENU PRINCIPAL ==============================================================='
+
+
+
+            Catch ex As Exception
+                MsgBox("Erreur affichage langue | Error display language", MsgBoxStyle.Critical, Me.Name & "/GestionLangue")
+            Finally
+                Bloc.Clear()
+            End Try
+
+        End If
+
+    End Sub
+
+    Private Sub GestionUnites()
+
+    End Sub
+
+    Private Sub GestionStyle()
+
+    End Sub
+
+    Private Sub AfficherPoutreEnCours()
+
+    End Sub
+
+#End Region
+
+#Region "===FERMETURE==="
+
+    Public Sub TraitementSaisie()
+
+    End Sub
+
+    Private Sub btn_OK_Click(sender As Object, e As EventArgs) Handles btn_OK.Click
+        Dim lModif As Boolean = False
+        If ValideSaisieFenetre() Then
+
+            TransfertSaisie(lModif)
+
+            If lModif Then
+
+            End If
+            Me.Close()
+        End If
+    End Sub
+
+
+    Private Function ValideSaisieFenetre() As Boolean
+        Return True
+    End Function
+
+    Private Sub TransfertSaisie(ByRef lModif As Boolean)
+
+    End Sub
+
+#End Region
+
+#Region " Dessins "
+
+
+
+#End Region
+
+#Region " Evènements "
+
+
+#End Region
+
+#Region " Evènements saisie "
+
+
+#End Region
+End Class
