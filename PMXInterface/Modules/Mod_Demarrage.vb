497f63f5fb941c1331def2a7bcfa7910bb1e7749
Pierre-Olivier MARTIN (CTICM) <pomartin@cticm.com>
2025-01-30 13:17:44 +0100

    Version V1.00 beta 3

--- PMXInterface/Modules/Mod_Demarrage.vb
+++ PMXInterface/Modules/Mod_Demarrage.vb
@@ -130,7 +130,7 @@ Public Module Mod_Demarrage
         '--> Réglages CTICM/AM
 
         LogicielInfo.Maitre = EnuMaitre.CTICM
-        LogicielInfo.Maitre = EnuMaitre.ArcelorMittal
+        'LogicielInfo.Maitre = EnuMaitre.ArcelorMittal
 
         InitialiseReglagesLogiciel()
         InitialiseVersion()
